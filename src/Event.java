f53ec3ba2542f9ea87eb5fed324ff5e9e5fc9c96
chenhaoheihei <x00217230115@njit.edu.cn>
2024-03-22 22:33:36 +0800

    CHANGE

--- src/Event.java
+++ src/Event.java
@@ -1,81 +1,97 @@
-public class Event {
+
+
+public class Event{
+
+    public static final double insuranceCost = 1000.00;
+    public static final double baseEventCost= 5000;
+    public static final double tax = 0.3;
+
     private String eventID;
     private String eventName;
     private String eventLocation;
-    private String eventPointOfContact;
+    private String pointOfContact;
     private double eventCost;
+    
     private int totalParticipants;
     private int totalEventDays;
 
-
-    public static final double insuranceCost=1000.00;
-    public static final double tax=0.3;
-
-    public Event(String eventID, String eventName, String eventLocation, String eventPointOfContact,
-                 double eventCost, int totalParticipants, int totalEventDays) {
+    public Event(){}
+    
+    public Event(String eventID, String eventName, String eventLocation, String pointOfContact, int totalParticipants, int totalEventDays){
         this.eventID = eventID;
         this.eventName = eventName;
         this.eventLocation = eventLocation;
-        this.eventPointOfContact = eventPointOfContact;
-        this.eventCost = eventCost;
+        this.pointOfContact = pointOfContact;
         this.totalParticipants = totalParticipants;
         this.totalEventDays = totalEventDays;
+
     }
-    public String getEventID() {
+
+    public String getEventID(){
         return eventID;
     }
+
     public void setEventID(String eventID){
         this.eventID = eventID;
     }
-    public String getEventName() {
-        return eventID;
+
+    public String getEventName(){
+        return eventName;
     }
+
     public void setEventName(String eventName){
         this.eventName = eventName;
     }
-    public String geteventLocation() {
+
+    public String getEventLocation(){
         return eventLocation;
     }
-    public void seteventLocation(String eventLocation){
-        this.eventLocation = eventLocation;
-    }
-    public String geteventPointOfContact() {
-        return  eventPointOfContact;
-    }
-    public void seteventPointOfContact(String eventPointOfContact){
-        this.eventPointOfContact = eventPointOfContact;
-    }
-    public String geteventCost() {
-        return  eventPointOfContact;
-    }
-    public void seteventCost(double eventCost){
-        this.eventCost = eventCost;
-    }
-    public int gettotalParticipants() {
-        return  totalParticipants;
-    }
-    public void settotalParticipants(int totalParticipants){
-        this.totalParticipants = totalParticipants;
-    }
-    public int gettotalEventDays() {
-        return  totalEventDays;
-    }
-    public void setotalEventDays(int totalEventDays){
-        this.totalEventDays =totalEventDays;
-    }
-    public String toString() {
-        return "Event Details: \n" +
-                "Event ID: " + eventID + "\n" +
-                "Event Name: " + eventName + "\n" +
-                "Event Location: " + eventLocation + "\n" +
-                "Event Point of Contact: " + eventPointOfContact + "\n" +
-                "Total Participants: " + totalParticipants + "\n" +
-                "Total Event Days: " + totalEventDays + "\n" +
-                "Event Cost: $" + eventCost;
- 
-    }
-    public void calculateEventCost(){
-        this.eventCost = eventCost + (eventCost * tax) + insuranceCost;
+
+    public void setEventLocation(String location){
+        this.eventLocation = location;
     }
 
+    public String getPointOfContact(){
+        return pointOfContact;
+    }
+
+    public void setPointOfContact(String pointOfContact){
+        this.pointOfContact = pointOfContact;
+    }
+
+    /* public double getEventCost(){
+        return eventCost;
+    }
+
+ */    /* public void setEventCost(double eventCost){
+        this.eventCost = eventCost;
+    } */
+
+    public int getTotalParticipants(){
+        return totalParticipants;
+    }
+
+    public void setTotalParticipants(int totalParticipants){
+        this.totalParticipants = totalParticipants;
+    }
+
+    public int getTotalEventDays(){
+        return totalEventDays;
+    }
+
+    public double getEventCost(){
+        return eventCost;
+    }
+
+    public void setTotalEventDays(int totalEventDays){
+        this.totalEventDays = totalEventDays;
+    }
+
+    public String toString(){
+        return eventID + " " + eventName + " " + eventLocation + " " + pointOfContact + " " + eventCost + " " + totalParticipants + " " + totalEventDays;
+    }
+
+    public void calculateEventCost(){
+        this.eventCost = baseEventCost + (baseEventCost * tax) + insuranceCost;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
-    public static void main(String[] args) throws Exception{
-       ConferenceEvent conferenceEvent = new ConferenceEvent("E001", "AI Conference", "Paris", "aha", 0, 0, 0, false, 0, false, 0, false, 0);
-       conferenceEvent.calculateEventCost();
-       System.out.println(conferenceEvent);
-        MusicConcertEvent event1 = new MusicConcertEvent(true, 1000.0);
-        System.out.println(event1.toString());
+    public static void main(String[] args) throws Exception {
+        ConferenceEvent conferenceEvent = new ConferenceEvent("E001", "AI Conference", "Paris", "Peng Tian", 50, 2, 25, 40, 60);
+        conferenceEvent.calculateEventCost();
+        System.out.println(conferenceEvent);
 
-        MusicConcertEvent event2 = new MusicConcertEvent(false, 0.0);
-        System.out.println(event2.toString());
+        PastEvent pastEvent = new PastEvent("E001", "AI Conference", "Paris", "Peng Tian", 50, 2);
+        pastEvent.calculateEventCost();
+        pastEvent.setPaymentDetails("Pending", true);
+        System.out.println(pastEvent);
     }
 }
